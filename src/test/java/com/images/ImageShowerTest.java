8458b4e69661ed364656dd63bea7c88db8ecf444
StasyanOi <iluvstrwrs@mail.ru>
2022-04-29 23:26:09 +0300

    Revert "added env variable for test"
    This reverts commit 195c74ba090f6c6d93147a92d1b97fdeabbf26e6.

--- src/test/java/com/images/ImageShowerTest.java
+++ src/test/java/com/images/ImageShowerTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@EnabledIfEnvironmentVariable(named = "DISPLAY", matches = ":0")
 class ImageShowerTest {
 
     ImageShower imageShower = new ImageShower();
